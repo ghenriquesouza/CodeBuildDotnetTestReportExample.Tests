25930c4523398f6f64498b80623a4d170fa16ae5
ghenriquesouza <ghenrique.souza@gmail.com>
2020-11-20 09:57:59 -0300

    Add project files.

---
+++ ExampleTests.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace CodeBuildDotnetTestReportExample.Tests
+{
+    public class ExampleTests
+    {
+        [Fact]
+        public void TestSuccess1()
+        {
+
+        }
+
+
+        [Fact]
+        public void TestSuccess2()
+        {
+
+        }
+
+        [Theory]
+        [InlineData("https://www.google.com")]
+        [InlineData("fffaaa")]
+        public void TestMalformedUri(string uri)
+        {
+            new Uri(uri);
+        }
+    }
+}
